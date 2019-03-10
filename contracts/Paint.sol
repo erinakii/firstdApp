80523e1619dceba5ca913fa59a962f5db72dc3c4
Erina Kii <erina.kii92@gmail.com>
2019-03-10 16:24:24 -0400

    added paint contract

---
+++ contracts/Paint.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.0;
+
+contract Paint {
+    //initial board
+    uint public boardSize = 20;
+    address[20][20] board;
+    string public color;
+    //painter 
+    address public painter;
+
+    constructor() public {
+        painter = msg.sender;
+    }
+
+    function getBoard() public view returns(address[20][20] memory){
+        return board;
+    }
+
+    function setColor(string clr) public {
+        color = clr;
+    }
+
+    function setPaint(uint8 x, uint8 y) public {
+        assert(x < boardSize);
+        assert(y < boardSize);
+
+        board[x][y] = msg.sender;
+    }
+}
